ab5db3c9648ba9fa7cf7982f91bba638f422defb
MateusCSH <91564533+MateusCSH@users.noreply.github.com>
2024-04-23 11:25:52 -0300

    Update ttstreamlit.py

--- ttstreamlit.py
+++ ttstreamlit.py
@@ -1,6 +1,5 @@
 import streamlit as st 
 import pandas as pd 
-import pyautogui as py
 import time
 
 
@@ -9,18 +8,6 @@ st.set_page_config(
                    layout='wide')
 
 py2 = st.file_uploader('Select seu arquivo',type='csv')
-
-if py2 is not None:
-    but = st.button('Click',type='primary')
-
-    if but:
-        py.PAUSE = 2
-        py.press('win')
-        py.write('chrome')
-        py.press('enter')
-
-    else:
-      st.info('Erro')
  
 
         
